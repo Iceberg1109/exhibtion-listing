2df0e05122d00e617ff17270e1dc1211bbd40f62
iceberg1109 <miguel.murillo1112@gmail.com>
2022-01-31 12:04:56 -0800

    create layout for pages

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import styles from "./layout.module.css";
+
+const Layout = ({
+  children,
+  home = false,
+}: {
+  children: any;
+  home?: boolean;
+}) => {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Views Exhibition Assessment" />
+        <meta name="og:title" content="Exhibition" />
+      </Head>
+      <header>
+        <div className={styles.pageTitle}>
+          <h2>Exhibitions!</h2>
+        </div>
+        {!home && (
+          <Link href="/">
+            <a>&#8592;Back to home</a>
+          </Link>
+        )}
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/exhibition/[id].tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Layout from "../../components/Layout";
+import styles from "../styles/Home.module.css";
+
+const ExhibitionDetail: NextPage = () => {
+  return <Layout>Exhibition</Layout>;
+};
+
+export default ExhibitionDetail;
